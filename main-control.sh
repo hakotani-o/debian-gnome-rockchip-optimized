6e5c33e34bdea0a311572b5882cbca33d22637a1
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-07-04 12:06:26 +0900

    Update main-control.sh

--- main-control.sh
+++ main-control.sh
@@ -14,7 +14,9 @@ sudo apt-get install debian-archive-keyring
 	sudo ./build_kernel_env.sh orangepi-5-rk3588s_defconfig $Uri $suite kernel
 	sudo ./meas-build-env.sh arm64 $1 $Uri $suite
 	#sudo ./meas-build-env.sh arm64 ubuntu $Uri $suite
+	echo "########################## ROOTFS START ############################"
 	sudo ./rootfs-bootstrap.sh arm64 $Uri $suite
+	echo "########################## ROOTFS END ############################"
 	sudo ./disk_image.sh arm64 orangepi-5 rk3588s-orangepi-5
 	sudo mv overlay/u-boot-rockchip.bin overlay/orangepi-5-u-boot-rockchip.bin
 	sudo ./build_kernel_env.sh orangepi-5-plus-rk3588_defconfig $Uri $suite u-boot
